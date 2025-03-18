8a852fcc8e8daf911a539efd69636292d290408c
Maimai10808 <331686549@qq.com>
2025-03-18 21:14:05 +0800

    Initial Commit

---
+++ Airbnb Clone/Airbnb_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Airbnb_CloneApp.swift
+//  Airbnb Clone
+//
+//  Created by mac on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Airbnb_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Airbnb CloneTests/Airbnb_CloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  Airbnb_CloneTests.swift
+//  Airbnb CloneTests
+//
+//  Created by mac on 3/18/25.
+//
+
+import Testing
+@testable import Airbnb_Clone
+
+struct Airbnb_CloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
